d6556b0790be47e9e91cd7d227b4176772cd9431
Rohan-devv <devv112004@gmail.com>
2025-10-01 03:18:25 +0530

    js practice

---
+++ index.js
@@ -0,0 +1,39 @@
+// What is javascript and where is it used?
+// JavaScript is a versatile, high-level programming language primarily used for web development. It enables interactive and dynamic content on websites, allowing developers to create features such as animations, form validations, and real-time updates. JavaScript can be executed in web browsers, making it an essential tool for front-end development. Additionally, with the advent of Node.js, JavaScript can also be used for server-side development, enabling developers to build full-stack applications using a single programming language.
+ 
+
+// Que-2 
+
+/* 
+what are the template literals in javascript?
+${expression},{},``
+
+embedd variable and expression in a string and make code more readable
+ 
+let a = 10;
+let b = 20;
+
+console.log(`The sum of ${a} and ${b} is ${a + b}`);
+
+*/
+
+/* Que-3
+
+
+// HOISTING-- where variable aor function moved on the top of their scope 
+
+//Type -1 function hoisting 
+greet();
+function greet(){
+    console.log("Hello world! ")
+}
+
+// type-2 variable hoisting
+console.log(a);
+var a = 10;
+
+// var me undefined and let const me refernce error
+*/
+
+// Que-4
+  
