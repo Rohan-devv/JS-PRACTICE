b5c2ebbc00efa2ff54bfb412189adb94e2986609
Rohan-devv <devv112004@gmail.com>
2025-10-10 03:41:25 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,21 @@
+/*
+Data Types in JavaScript
+
+1. Primitive Data Types:
+   - Number: Represents both integer and floating-point numbers.
+   - String: Represents a sequence of characters.
+   - Boolean: Represents a logical entity and can have two values: true and false.
+   - Undefined: A variable that has been declared but has not yet been assigned a value.
+   - Null: Represents the intentional absence of any object value.
+   - Symbol: A unique and immutable primitive value introduced in ES6.
+
+2. Reference Data Types:
+   - Object: A collection of key-value pairs.
+   - Array: A special type of object used for storing ordered collections.
+   - Function: A block of code designed to perform a particular task.
+
+
+   console.log(typeof null); // object (this is a known quirk in JavaScript)
+   console.log(typeof undefined); // undefined
+
+*/
