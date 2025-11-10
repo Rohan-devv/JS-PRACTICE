3bcb471a1c30fd945edb9adac475c46aa6934dbf
Rohan-devv <devv112004@gmail.com>
2025-11-11 01:54:56 +0530

    done for the day!!

---
+++ arrowfunction.js
@@ -0,0 +1,45 @@
+const user = {
+    username:"rohan",
+    price: 100,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} welcome to websitee `)
+        //console.log(this) // ye user object ko point karega
+    }
+}
+
+user.username = "Rohan Pal"
+user.welcomeMessage()
+
+//console.log(this) // ye global object ko point karega
+
+// browser me ye window object ko point karega
+
+function chai(){
+    let username = "rohan"
+    console.log(this.username) // undefined because function ke andar this global object ko point karega lekin global object me username defined nahi hai 
+}
+//chai()
+
+const arrowChai = () => {
+    let username = "rohan"
+    console.log(this.username)// ye bhi undefined dega because arrow function me bhi this global object ko point karega lekin global object me username defined nahi hai
+}
+//arrowChai()
+
+//+++++++++++++++++++++++ Arrow Function ++++++++++++++++++++++
+
+const addtwo = (num1, num2) => {
+    return num1 + num2
+}
+//console.log(addtwo(3,4));
+
+
+const addtwo = (num1, num2) => num1 + num2
+const addtwo = (num1, num2) => (num1 + num2)// react me bhot baar aise likhna padta hai
+
+// curly brace me return likhna jaruri hai agar function body me multiple line ho to
+
+
+
+const addtwo = (num1, num2) => ({username:"dfgh"})// object return karna ho to aise karna padta hai
---
+++ hoisting.js
@@ -0,0 +1,25 @@
+function one(){
+    const username = "Rohan"
+
+    function two(){
+        const website = "rohan.com"
+        console.log(username);
+    }
+    // console.log(website) // error
+    two()
+}
+console.log(username) // error
+one()
+
+// ++++++++++++++++++++++ Hoisting ++++++++++++++++++++++
+
+
+addOne(5)// ye kaam karega because function declaration me jo function hai wo upar jata hai
+ function addOne(num){
+    return num+1
+ }  
+
+addTwo(5) // ye error dega because function expression me jo function hai wo upar nahi jata
+ const addTwo = function(num){
+    return num+2
+ }
