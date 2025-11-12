d8dde5c828d3f6a3602b187b73f8520791c0c149
Rohan-devv <devv112004@gmail.com>
2025-11-13 02:22:16 +0530

    Codeflow

---
+++ codeexecution.js
@@ -0,0 +1,10 @@
+// Javascript Execution Context and Call Stack
+
+/*
+javascript ek single threaded language hai jiska matlab hai ki ek samay me sirf ek kaam hi kar sakta hai
+1--> Global Execution Context
+2--> Function Execution Context
+
+Jab bhi javascript code run hota hai to ek hota hai Memory creation phase and ek hota hai execution phase 
+
+*/
