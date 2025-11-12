4a39725ed4ec249c6950c027966c390b7c65d9c4
Rohan-devv <devv112004@gmail.com>
2025-11-13 02:43:43 +0530

    Basic almost complete

---
+++ imploop.js
@@ -0,0 +1,37 @@
+const arr = [1, 2, 3, 4, 5]
+
+arr.forEach( (item) => {
+    console.log(item);
+})
+
+/*
+callback function kya hota hai?
+Jab bhi hum kisi function ke andar ek function ko argument ke roop me pass karte hai to us function ko callback function kehte hai
+bss diff naam hai esko naam nhi dete hai kyuki ye function sirf ek baar use hota hai to isko naam dene ki jarurat nahi hoti hai
+*/ 
+
+arr.forEach( (item,index,arr) => {
+    console.log(item,index,arr);
+})
+
+const arrOfObject = [
+
+    {
+        username:"rohan", 
+        price:1030
+    },
+    {
+        username:"rohaf", 
+        price:1030
+    },
+    {
+        username:"rog", 
+        price:1040
+    },
+    
+
+]
+
+arrOfObject.forEach( (item) => {
+    console.log(item.username);
+})
