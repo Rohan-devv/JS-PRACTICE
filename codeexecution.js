f0cae941982f15bf9a1b697c00bb1085abdad596
Rohan-devv <devv112004@gmail.com>
2025-11-13 02:29:24 +0530

    ghj

--- codeexecution.js
+++ codeexecution.js
@@ -7,4 +7,8 @@ javascript ek single threaded language hai jiska matlab hai ki ek samay me sirf 
 
 Jab bhi javascript code run hota hai to ek hota hai Memory creation phase and ek hota hai execution phase 
 
+call stack kya hota hai?
+Call stack ek aisi jagah hai jaha pe javascript apne function ko track karta hai ki konsa function abhi chal raha hai aur konsa function call hua hai
+LIFO --> Last In First Out follow karta hai
+
 */
