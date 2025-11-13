f92ce1b6fcb1ce883bc86d58ba242e2cc820b51e
Rohan-devv <devv112004@gmail.com>
2025-11-14 01:53:24 +0530

    shoppingCart Love it !!

--- MostImpLoops.js
+++ MostImpLoops.js
@@ -47,7 +47,45 @@ const newNums = myNums.map((num)=> {
 
 
 // ++++++++++++++++++++ reduce ka use ++++++++++++++++++++
-
+/*
 const myNums=[1,2,3]
 
+const sum = myNums.reduce( (acc, currval) => {
+    console.log(`acc:${acc} and currval: ${currval}`)
+    return acc + currval
 
+}, 0)
+
+
+console.log(sum);
+*/
+const cart = [
+  {
+    id: 1,
+    name: "Wireless Earbuds",
+    price: 1499,
+    quantity: 2,
+    category: "Electronics",
+    image: "earbuds.jpg"
+  },
+  {
+    id: 2,
+    name: "Men's T-Shirt",
+    price: 499,
+    quantity: 1,
+    category: "Clothing",
+    size: "L",
+    image: "tshirt.jpg"
+  },
+  {
+    id: 3,
+    name: "Sports Water Bottle",
+    price: 299,
+    quantity: 3,
+    category: "Sports",
+    image: "bottle.jpg"
+  }
+];
+const totalAmount = cart.reduce( (acc, item) => {
+     return acc + (item.price * item.quantity)
+}, 0)
