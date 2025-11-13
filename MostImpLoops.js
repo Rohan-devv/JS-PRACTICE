e5c44af93f42af11c7e3ef69a186f0210bdf919d
Rohan-devv <devv112004@gmail.com>
2025-11-14 01:44:55 +0530

    reduce ....

--- MostImpLoops.js
+++ MostImpLoops.js
@@ -1,4 +1,4 @@
-const myNums=[1,2,3,4,5,6,7,8,9] 
+//const myNums=[1,2,3,4,5,6,7,8,9] 
 
   //const newNums = myNums.filter( (num) => num > 4)
 
@@ -19,8 +19,35 @@ myNums.forEach( (num) => {
     }
 
 })
-    */
+*/
+
+// +++++++++++++++ filter ka use ++++++++++++++++++
+/*
+const users = [
+  { id: 1, name: "Rohan", age: 22 },
+  { id: 2, name: "Amit", age: 24 },
+  { id: 3, name: "Sara", age: 21 }
+];
+
+ const result = users.filter ((items) => items.age >21)
 
   
-  console.log(newNums);
-  
+ console.log(result);
+*/
+
+ //++++++++++++++++++++ map ka use ++++++++++++++++++++
+
+/*
+const myNums=[1,2,3,4,5,6,7,8,9]
+
+const newNums = myNums.map((num)=> {
+    return num+10
+})
+*/
+
+
+// ++++++++++++++++++++ reduce ka use ++++++++++++++++++++
+
+const myNums=[1,2,3]
+
+
