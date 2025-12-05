a82889b0983ae2fbbb9af16c66a14c3e06bd6b31
Rohan-devv <devv112004@gmail.com>
2025-12-06 05:15:25 +0530

    personla learnings ..........

---
+++ myLearnings/Quotes and NO quotes.js
@@ -0,0 +1,22 @@
+/*
+
+⭐ SUPER SIMPLE RULES TO REMEMBER
+✔ RULE 1 → Expression ONLY (no HTML) → No_QUOTES
+innerHTML = variable
+innerHTML = variable.join(", ")
+innerHTML = number
+
+✔ RULE 2 → HTML likhna ho → BACKTICKS
+innerHTML = `<h2>Hello</h2>`
+innerHTML = `<p>${score}</p>`
+
+✔ RULE 3 → Mix JS + HTML → BACKTICKS ALWAYS
+innerHTML = `<span>${prevGuess.length} guesses</span>`
+
+✔ RULE 4 → FIXED string, NO JS, small text → Quotes allowed
+innerHTML = "<h1>Game Over</h1>"
+
+
+But modern JS me backticks recommended.
+
+*/
